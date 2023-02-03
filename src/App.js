7d338f52a99c08aee1e9c8bd42ec2c86008e10ea
marcel <marcelinocost69@gmail.com>
2023-02-03 17:47:30 -0300

    começando com reactjs

--- src/App.js
+++ src/App.js
@@ -1,22 +1,28 @@
-import logo from './logo.svg';
+import {
+  addDays,
+  addMonths,
+  addYears,
+  closestTo,
+  format,
+  subDays,
+} from 'date-fns';
+import ptBR from 'date-fns/locale/pt-BR';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [diaAtual, setDiaAtual] = useState(new Date());
+
+  const one = format(diaAtual, 'dd', { locale: ptBR });
+  const two = format(diaAtual, 'MMMM', { locale: ptBR });
+  const three = format(diaAtual, 'yyyy', { locale: ptBR });
+  const four = format(diaAtual, 'cccc', { locale: ptBR });
+  const five = format(diaAtual, 'HH', { locale: ptBR });
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>{`Maceió-AL, ${four}, ${one} de ${two} de ${three} às ${five} horas`}</p>
       </header>
     </div>
   );
