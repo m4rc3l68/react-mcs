5e11f2930f980a1d554d6dc99ddad37497a89ae2
marcel <marcelinocost69@gmail.com>
2023-02-11 19:16:38 -0300

    commit  - 05

--- src/index.js
+++ src/index.js
@@ -5,16 +5,16 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import Componente from './pages/ContadorFuncao';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <Componente />
-  </React.StrictMode>,
-  document.getElementById('root'),
-);
-
-// const root = ReactDOM.createRoot(document.getElementById('root'));
-// root.render(
+// ReactDOM.render(
 //   <React.StrictMode>
 //     <Componente />
 //   </React.StrictMode>,
+//   document.getElementById('root'),
 // );
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <Componente />
+  </React.StrictMode>,
+);
