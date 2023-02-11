e731720e37a1d5800e56836c30367d507c5ac57f
marcel <marcelinocost69@gmail.com>
2023-02-11 17:59:38 -0300

    commit - 04

--- src/index.js
+++ src/index.js
@@ -1,9 +1,8 @@
 // import React from 'react';
-// import ReactDOM from 'react-dom/client';
 // import App from './App';
 
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import Componente from './pages/ContadorFuncao';
 
 ReactDOM.render(
